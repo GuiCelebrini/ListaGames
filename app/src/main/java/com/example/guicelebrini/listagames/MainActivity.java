3b9bef95bec210098e1dc7354185d36576f80fc5
GuiCelebrini <celebrinig@gmail.com>
2021-05-12 17:34:33 -0300

    Mais jogos adicionados

--- app/src/main/java/com/example/guicelebrini/listagames/MainActivity.java
+++ app/src/main/java/com/example/guicelebrini/listagames/MainActivity.java
@@ -16,41 +16,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private RecyclerView recyclerView;
-    private List<Game> listaGames = new ArrayList<>();
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        montarLista();
-
-        /*recyclerView = findViewById(R.id.recyclerView);
-
-        Adapter adaptador = new Adapter(listaGames);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
-        recyclerView.setAdapter(adaptador);*/
-
-
-
     }
 
-    public void montarLista(){
-        Game game1 = new Game("God of War", "2018","94", "https://cdn.pocket-lint.com/r/s/1200x/assets/images/143881-games-review-review-god-of-war-screens-image1-hcyb5z2mg0.jpg");
-        listaGames.add(game1);
-
-        Game game2 = new Game("The Last of Us Part II", "2020", "93", "https://1.bp.blogspot.com/-EEeSRpUuYmQ/XuLnaVAvHJI/AAAAAAAAWBw/GFJzSGtnI4Q8uidPhbdcti36kx-JE-5FgCLcBGAsYHQ/s1600/CAPA.png");
-        listaGames.add(game2);
-
-        Game game3 = new Game("Ghost of Tsushima", "2020", "83","https://m.media-amazon.com/images/S/aplus-media/vc/877cb42f-1e4e-4c78-89e8-3b2204f48df0.__CR0,0,1464,600_PT0_SX1464_V1___.jpg");
-        listaGames.add(game3);
-    }
 }
--- app/src/main/java/com/example/guicelebrini/listagames/fragments/GamesFragment.java
+++ app/src/main/java/com/example/guicelebrini/listagames/fragments/GamesFragment.java
@@ -56,14 +56,35 @@ public class GamesFragment extends Fragment {
     }
 
     public void montarLista(){
-        Game game1 = new Game("God of War", "2018","94", "https://cdn.pocket-lint.com/r/s/1200x/assets/images/143881-games-review-review-god-of-war-screens-image1-hcyb5z2mg0.jpg");
+        Game game1 = new Game("Ocarina of Time", "1998","99", "https://www.wasd.pt/wp-content/uploads/2018/01/zelda-wasd-ocarina-time.png");
         listaGames.add(game1);
 
-        Game game2 = new Game("The Last of Us Part II", "2020", "93", "https://1.bp.blogspot.com/-EEeSRpUuYmQ/XuLnaVAvHJI/AAAAAAAAWBw/GFJzSGtnI4Q8uidPhbdcti36kx-JE-5FgCLcBGAsYHQ/s1600/CAPA.png");
+        Game game2 = new Game("Tony Hawk II", "2000", "98", "https://upload.wikimedia.org/wikipedia/pt/a/a9/Tony_Hawk%27s_Pro_Skater_2_n64_cover.png");
         listaGames.add(game2);
 
-        Game game3 = new Game("Ghost of Tsushima", "2020", "83","https://m.media-amazon.com/images/S/aplus-media/vc/877cb42f-1e4e-4c78-89e8-3b2204f48df0.__CR0,0,1464,600_PT0_SX1464_V1___.jpg");
+        Game game3 = new Game("Grand Theft Auto IV", "2008", "98","https://upload.wikimedia.org/wikipedia/pt/thumb/9/91/Grand_Theft_Auto_IV_capa.png/270px-Grand_Theft_Auto_IV_capa.png");
         listaGames.add(game3);
+
+        Game game4 = new Game("Soul Calibur", "1999", "98","https://dropsdejogos.uai.com.br/wp-content/uploads/sites/10/2020/08/soul-950x695.jpg");
+        listaGames.add(game4);
+
+        Game game5 = new Game("Super Mario Galaxy", "2007","97", "https://uploads.jovemnerd.com.br/wp-content/uploads/2019/12/super-mario-galaxy-2-e-o-melhor-jogo-da-decada-segundo-o-metacritic.jpg");
+        listaGames.add(game5);
+
+        Game game6 = new Game("Super Mario Galaxy 2", "2010", "97", "https://d17lbu6bbzbdc8.cloudfront.net/wp-content/uploads/2020/08/23194420/super-mario-galaxy.original-scaled.jpg");
+        listaGames.add(game6);
+
+        Game game7 = new Game("Red Dead Redemption 2", "2018", "97","https://images4.kabum.com.br/produtos/fotos/140584/red-dead-redemption-2-rockstar-_1611085792653_g.jpg");
+        listaGames.add(game7);
+
+        Game game8 = new Game("Grand Theft Auto V", "2013", "97","https://cdn-products.eneba.com/resized-products/t0zqmqhdcxppyol3mtlg_350x200_1x-0.jpg");
+        listaGames.add(game8);
+
+        Game game9 = new Game("Breath of the Wild", "2017", "97","https://i.pinimg.com/originals/76/e2/c9/76e2c94e57bf13944928c97cdbca901d.jpg");
+        listaGames.add(game9);
+
+        Game game10 = new Game("Tony Hawk III", "2001", "97","https://images-na.ssl-images-amazon.com/images/I/5183anCzIkL._AC_SX425_.jpg");
+        listaGames.add(game10);
     }
 
 }
